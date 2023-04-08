2c1c908e95d3519999f1a82a932214712f04ef24
Dhruv <dhruvsuvarna2019@gmail.com>
2023-04-08 17:48:26 +0530

    orphanage dashboard updated

--- Orphanage/urls.py
+++ Orphanage/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.orphanage, name='orphanage')
+    path('', views.orphanage, name='orphanage'),
+    path('view_donations', views.view_donations, name='view_donations'),
+    path('add_details', views.add_details, name='add_details'),
+    path('view_details', views.view_details, name='view_details')
 ]
--- Orphanage/views.py
+++ Orphanage/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def orphanage(request):
 
     return render(request,'orphanage.html')
+
+def view_donations(request):
+    donations = [ 'donation 1', 'donation 2', 'donation 3']
+    return render(request, 'o_view_donations.html', {'donations': donations})
+
+def add_details(request):
+    if request.method == 'POST':
+        o_name = request.POST['o_name']
+        if request.FILES == 'o_img':
+            o_img = request.FILES['o_img']
+    return render(request, 'o_add_details.html')
+
+
+def view_details(request):
+    details = [ 'detail 1', 'detail 2', 'detail 3']
+    return render(request, 'o_view_details.html', {'details': details})
